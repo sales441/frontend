727ab84a0c9e0ef0dc058fef9a100ff44ae589af
sales441 <sales@depilcompany.com>
2025-10-10 15:07:20 -0400

    Delete Vikingo-Ads-Brain-Frontend-Fix2 directory

--- Vikingo-Ads-Brain-Frontend-Fix2/src/pages/IA.jsx
+++
@@ -1,5 +0,0 @@
--import axios from 'axios';
-+import API from '../services/api';
-
--const res = await axios.post('http://localhost:5000/api/ai/suggest', { brand: 'Depil Bella' });
-+const res = await API.post('/ai/suggest', { brand: 'Depil Bella' });
