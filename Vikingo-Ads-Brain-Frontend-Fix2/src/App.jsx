239db708c56c027d22eace41a94c7b2fcbeea32a
sales441 <sales@depilcompany.com>
2025-10-10 15:29:28 -0400

    Delete Vikingo-Ads-Brain-Frontend-Fix2 directory

--- Vikingo-Ads-Brain-Frontend-Fix2/src/App.jsx
+++
@@ -1,3 +0,0 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard.jsx';
-export default function App(){return(<BrowserRouter><Routes><Route path='/' element={<Dashboard/>}/></Routes></BrowserRouter>);}
