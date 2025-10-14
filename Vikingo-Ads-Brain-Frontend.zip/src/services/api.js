f8c74e86b94f29bd6af585cb883baff658d3f06b
sales441 <sales@depilcompany.com>
2025-10-14 12:30:26 -0400

    Update api.js

--- Vikingo-Ads-Brain-Frontend.zip/src/services/api.js
+++ Vikingo-Ads-Brain-Frontend.zip/src/services/api.js
@@ -1,9 +1,2 @@
-﻿import axios from "axios";
-
-const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_BASE_URL ||
-    "https://backhand-production-bc90.up.railway.app/api",
-});
-
-export default API;
+PS C:\Users\sales> $body = @{ email='ivan@depilcompany.com'; password='Isabella0101@' } | ConvertTo-Json
+PS C:\Users\sales> Invoke-RestMethod -Uri 'https://backhand-production-bc90.up.railway.app/api/auth/login' -Method Post -Body $body -ContentType 'application/json'
